4117257142d02141ce88cd3f23b5201edb475e10
Paola Zanella <pzanella@edu.univali.br>
2024-05-11 17:44:03 -0300

    botao de tirar foto esta funciando,porem nao esta mostrando a foto. botao de selecinar foto falta ser feito

--- app/src/main/java/com/paolazanella/loginapp/terceiraTelaActivity.kt
+++ app/src/main/java/com/paolazanella/loginapp/terceiraTelaActivity.kt
@@ -1,14 +1,46 @@
 package com.paolazanella.loginapp
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class terceiraTelaActivity : AppCompatActivity() {
+
+    lateinit var btn: Button
+    lateinit var img: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_terceira_tela)
 
+        btn = findViewById(R.id.tirar_foto_btn)
+        img = findViewById(R.id.imagem)
+
+        btn.setOnClickListener {
+            val tirarFoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            resultContract.launch(tirarFoto)
+        }
     }
+
+    private val resultContract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val bitmap = data?.extras?.get("data") as? Bitmap
+            if (bitmap != null) {
+                // Mostrar a imagem na ImageView
+                img.setImageBitmap(bitmap)
+            } else {
+                // Se a bitmap for nula, há algum problema na captura da foto
+                Log.e("terceiraTelaActivity", "Erro: Foto não foi capturada corretamente.")
+            }
+        }
+    }
+
 }

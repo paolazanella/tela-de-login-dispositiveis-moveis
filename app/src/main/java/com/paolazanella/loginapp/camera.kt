bcd50b64e0fc454714e0e541f4198e145e711937
Paola Zanella <pzanella@edu.univali.br>
2024-05-12 11:11:01 -0300

    o teste da camera esta funciando agora testar desenvolveer o selecinar imagem

--- app/src/main/java/com/paolazanella/loginapp/camera.kt
+++ app/src/main/java/com/paolazanella/loginapp/camera.kt
@@ -1,20 +1,36 @@
 package com.paolazanella.loginapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+
 
 class camera : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_camera)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnCamera = findViewById<Button>(R.id.btnCamera)
+
+
+        btnCamera.setOnClickListener{
+            startForResult.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
         }
     }
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                val imageBitmap = intent?.extras?.get("data") as Bitmap
+                val imageView = findViewById<ImageView>(R.id.imageView)
+                imageView.setImageBitmap(imageBitmap)
+            }
+        }
 }
